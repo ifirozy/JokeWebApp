3bc2ff8e4ab320608f6b08723b039c405913c623
Iffat Firozy <ifirozy@gmail.com>
2022-05-22 13:56:17 +0600

    Add project files.

---
+++ JokeWebApp/Models/Jokes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JokeWebApp.Models
+{
+    public class Jokes
+    {
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+
+
+        public Jokes()
+        {
+
+        }
+    } 
+
+}
+
+
